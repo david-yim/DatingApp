6c696a6421bf92027e8aa060b4fb960015cde40e
David Yim <david.yim1@outlook.com>
2022-05-03 15:26:59 -0400

    section 4

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { User } from './Models/user';
+import { AccountService } from './_services/account.service';
 
 
 @Component({
@@ -11,17 +13,14 @@ export class AppComponent implements OnInit {
   title = 'The Very Good Dating App';
   users: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(private accountService: AccountService) {}
 
   ngOnInit() {
-    this.getUsers();
+    this.setCurrentUser();
   }
 
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe(response => {
-      this.users = response;
-    }, error => {
-      console.log(error);
-    })
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
   }
 }
